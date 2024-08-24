5687a55df93897d632c039c1d50c25a45b5ad5ec
flogychan <59194390+flogychannn@users.noreply.github.com>
2024-08-25 08:47:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
-    const queryString = window.location.search;
-    const params = new URLSearchParams(queryString);
-    const paramValue = params.get('text');
+$(function () {
+    $('#button').click(function () {
+        const queryString = window.location.search;
+        const params = new URLSearchParams(queryString);
+        const paramValue = params.get('text');
 
-    navigator.clipboard.writeText(paramValue);
+        navigator.clipboard.writeText(paramValue);
 
-    $('.msg').fadeIn("slow", function () {
-        $(this).delay(2000).fadeOut("slow");
+        $('.msg').fadeIn("slow", function () {
+            $(this).delay(2000).fadeOut("slow");
+        });
     });
 });
