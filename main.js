2d1a8e654d4ba151c3df23ffb67c69a9f86d5917
flogychan <59194390+flogychannn@users.noreply.github.com>
2024-08-25 08:21:20 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const paramValue = params.get('text');
+
+    navigator.clipboard.writeText(paramValue);
+
+    $('.msg').fadeIn("slow", function () {
+        $(this).delay(2000).fadeOut("slow");
+    });
+});
